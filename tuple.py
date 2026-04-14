07d73fc6aabfd5f5536807102d0a9b83368acd57
ImranAliMagsi78 <magsiimran101@gmail.com>
2026-04-14 10:03:14 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,24 @@
+# data = (1,3,4,5,6,7,8)
+# print(data)
+
+# data = (1,2,3,4,5,6,7,8,9,10,11)
+# for i in data:
+#     print(i)
+
+# data=(1,2,3,4,5,6,7,8,913,12,14,35,24,26,79,43,455,-12)
+# print(data.count(14))
+# print(data.index(26))
+# print(len(data))
+# print(max(data))
+# print(min(data))
+# print(sum(data))
+
+# set_data={1,2,3,4,5,6,7,8,9,1,2,3,4}
+# print(set_data)
+
+set_data={1,2,3,4,5,6,7,8,9,1,2,3,4}
+for i in set_data:
+    print(i)
+
+
+
