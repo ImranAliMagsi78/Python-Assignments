77ac315a578e11c6b689dbcce3f3555cc3d883c4
ImranAliMagsi78 <magsiimran101@gmail.com>
2026-04-14 07:07:43 +0100

    Add files via upload

---
+++ list_functions.py
@@ -0,0 +1,8 @@
+nums=[2,3,6,3,6,85,34,45,6,32,45,66,89,34,56]
+print("Before Append" , nums)
+nums.append(560)
+print("After Append" , nums)
+nums.sort()
+print("After Sort" , nums)
+nums.reverse()
+print("After Reverse" , nums)
---
+++ list_loop.py
@@ -0,0 +1,7 @@
+names=["Muzamil" , "Mubashir" , "Mudasir" , "Muhhamad" , "Ali" , "Hassan" , "Ahmad" , "Hasnain" ]
+for name in names:
+    print(name)
+
+ages=[1,2,37,52,13,78,26,98]
+for age in ages:
+    print(age)
