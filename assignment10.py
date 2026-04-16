972919fc64a252a6ff35210fecadfe368729193a
ImranAliMagsi78 <magsiimran101@gmail.com>
2026-04-16 09:54:15 +0500

    Add files via upload

---
+++ assignment10.py
@@ -0,0 +1,40 @@
+# data={
+#     "name":"Ali",
+#     "Marks":80,
+#     "Name":"Sara",
+#     "Marks":90,
+#     "Name":"Ahmad",
+#     "Marks":75
+# }
+# #print(data.key())
+# for key in data.keys():
+#     print(key)
+# for value in data.values():
+#     print(value)
+# print(max(Marks.values()))
+
+# marks = {
+#     "Ali": 80,
+#     "Ahmed": 75,
+#     "Sara": 90,
+    
+# }
+
+
+# print(max(marks.values()))
+
+# top_student = max(marks, key=marks.get)
+# print(top_student)
+
+# top_student = max(marks, key=marks.get)
+# print(top_student, marks[top_student])
+
+student = {
+                 "name":"ALI",
+                 "age":20,
+                 "marks":80
+}
+
+print(student.keys())
+print(student.values())
+print(student.items())
---
+++ assignment9.py
@@ -0,0 +1,22 @@
+numbers = [1,2,3,4,5,6]
+
+# print(numbers)
+# print(max(numbers))
+# print(min(numbers))
+# print(sum(numbers))
+
+# for i in numbers:
+#     if i % 2 == 0:
+#         print(i)
+
+
+t = (10,20,30,10,20)
+
+print(t)
+print(t.count(10))
+
+
+
+
+
+
